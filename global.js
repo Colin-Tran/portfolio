0e3b49a8454bd63d0c4d9759b9e620f6cdd6985b
Colin-Tran <colintran04@gmail.com>
2026-04-17 15:08:42 -0700

    DARK MODE

--- global.js
+++ global.js
@@ -12,6 +12,36 @@ function $$(selector, context = document) {
 
 // currentLink?.classList.add('current');
 
+document.body.insertAdjacentHTML(
+  'afterbegin',
+  `
+	<label class="color-scheme">
+		Theme:
+		<select>
+			<option value="light dark">Automatic</option>
+      <option value="light">Light</option>
+      <option value="dark">Dark</option>
+		</select>
+	</label>`,
+);
+
+let select = document.querySelector(".color-scheme select");
+
+select.addEventListener("input", function (event) {
+  console.log("color scheme changed to", event.target.value);
+
+  document.documentElement.style.setProperty('color-scheme', event.target.value);
+  localStorage.colorScheme = event.target.value;
+});
+
+if ("colorScheme" in localStorage) {
+  let saved = localStorage.colorScheme;
+
+  document.documentElement.style.setProperty("color-scheme", saved);
+
+  select.value = saved;
+}
+
 const BASE_PATH =
   location.hostname === "localhost" || location.hostname === "127.0.0.1"
     ? "/"

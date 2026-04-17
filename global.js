e0d75b08a5aa692637259c5798ed55ef6e2941a6
Colin-Tran <colintran04@gmail.com>
2026-04-17 14:38:38 -0700

    step 3.1 -- adding nav back

--- global.js
+++ global.js
@@ -4,10 +4,37 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-let navLinks = $$("nav a");
+// let navLinks = $$("nav a");
 
-let currentLink = navLinks.find(
-  (a) => a.host === location.host && a.pathname === location.pathname,
-);
+// let currentLink = navLinks.find(
+//   (a) => a.host === location.host && a.pathname === location.pathname,
+// );
 
-currentLink?.classList.add('current');
+// currentLink?.classList.add('current');
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+  ? "/"                  // Local server
+  : "/website/";         // GitHub Pages repo name
+
+  
+let pages = [
+  { url: "", title: "Home" },
+  { url: "projects/", title: "Projects" },
+  { url: "contact/", title: "Contact" },
+  { url: "resume/", title: "Resume" },
+  { url: "https://github.com/Colin-Tran", title: "GitHub" },
+];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+  let url = p.url;
+  let title = p.title;
+  url = !url.startsWith('http') ? BASE_PATH + url : url;
+  
+  // next step: create link and add it to nav
+
+  // Create link and add it to nav
+  nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+}
+

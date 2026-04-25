e0ca9a7b59238a374e87ca329dd9032f5ce63c2e
Colin-Tran <colintran04@gmail.com>
2026-04-24 23:55:52 -0700

    lab 4 changes (forgot to commit each step)

--- global.js
+++ global.js
@@ -84,3 +84,65 @@ for (let p of pages) {
 
   nav.append(a);
 }
+
+export async function fetchJSON(url) {
+  try {
+    // Fetch the JSON file from the given URL
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch projects: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    return data;
+
+  } catch (error) {
+    console.error('Error fetching or parsing JSON data:', error);
+  }
+}                                                                                                     
+
+export function renderProjects(projects, containerElement, headingLevel = "h2") {
+
+  // check container
+  if (!containerElement) {
+    console.error("Invalid container element");
+    return;
+  }
+
+  containerElement.innerHTML = "";
+  
+  // check if headings are valid
+  const validHeadings = ["h1", "h2", "h3", "h4", "h5", "h6"];
+
+  if (!validHeadings.includes(headingLevel)) {
+    headingLevel = "h2";
+  }
+
+  if (!projects || projects.length === 0) {
+    containerElement.innerHTML = "<p>No projects available.</p>";
+    return;
+  }
+
+  for (let project of projects) {
+    const article = document.createElement("article");
+
+    // use defaults if properties are missing
+    const title = project.title || "Untitled Project";
+    const description = project.description || "No description available.";
+    const image = project.image || "https://vis-society.github.io/labs/2/images/empty.svg";
+
+    article.innerHTML = `
+      <${headingLevel}>${title}</${headingLevel}>
+      <img src="${image}" alt="${title}">
+      <p>${description}</p>
+    `;
+
+    containerElement.appendChild(article);
+  }
+}
+
+export async function fetchGitHubData(username) {
+  // return statement here
+  return fetchJSON(`https://api.github.com/users/${username}`);
+}

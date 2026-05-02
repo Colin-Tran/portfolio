d4b47aad11f880a1894498eaa645a3ac82a0280a
Colin-Tran <colintran04@gmail.com>
2026-05-01 19:58:17 -0700

    added pie chart (js) and deleted unnecessary code

--- projects/projects.js
+++ projects/projects.js
@@ -1,4 +1,5 @@
 import { fetchJSON, renderProjects } from '../global.js';
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
 const projects = await fetchJSON('../lib/projects.json');
 
@@ -9,3 +10,38 @@ renderProjects(projects, projectsContainer, 'h2');
 const titleElement = document.querySelector('.projects-title');
 
 titleElement.textContent = `${projects.length} Projects`;
+
+// let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+
+// let arc = arcGenerator({
+//   startAngle: 0,
+//   endAngle: 2 * Math.PI,
+// });
+
+// d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
+
+let colors = d3.scaleOrdinal(d3.schemeTableau10);
+let data = [
+  { value: 1, label: 'apples' },
+  { value: 2, label: 'oranges' },
+  { value: 3, label: 'mangos' },
+  { value: 4, label: 'pears' },
+  { value: 5, label: 'limes' },
+  { value: 5, label: 'cherries' },
+];
+
+
+let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+
+let sliceGenerator = d3.pie().value((d) => d.value);    
+
+let arcData = sliceGenerator(data);
+let arcs = arcData.map(d => arcGenerator(d));
+
+
+arcs.forEach((arc, idx) => {
+  d3.select('#projects-pie-plot')   
+    .append('path')
+    .attr('d', arc)
+    .attr('fill', colors(idx));
+});

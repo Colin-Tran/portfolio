ad7d0346edf5113847f67d07234a673b7ef90f18
Colin-Tran <colintran04@gmail.com>
2026-05-01 21:15:25 -0700

    added serach bar

--- projects/projects.js
+++ projects/projects.js
@@ -20,6 +20,9 @@ titleElement.textContent = `${projects.length} Projects`;
 
 // d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
 
+
+// Pie Chart Generation for Projects by Year
+
 let colors = d3.scaleOrdinal(d3.schemeTableau10);
 // let data = [
 //   { value: 1, label: 'apples' },
@@ -29,7 +32,6 @@ let colors = d3.scaleOrdinal(d3.schemeTableau10);
 //   { value: 5, label: 'limes' },
 //   { value: 5, label: 'cherries' },
 // ];
-
  
 let rolledData = d3.rollups(
   projects,
@@ -67,4 +69,52 @@ data.forEach((d, idx) => {
     .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`);
 });
 
+// add search field
+let query = '';
 
+let searchInput = document.querySelector('.searchBar');
+
+searchInput.addEventListener('change', (event) => {
+  // update query value
+  query = event.target.value.toLowerCase();
+
+  // filter projects
+  let filteredProjects = projects.filter((project) => {
+    let values = Object.values(project).join('\n').toLowerCase();
+    return values.includes(query.toLowerCase());
+  });
+  // render filtered projects
+  renderProjects(filteredProjects, projectsContainer, 'h2');
+});
+
+// Refactor all plotting into one function
+function renderPieChart(projectsGiven) {
+  // re-calculate rolled data
+  let newRolledData = d3.rollups(
+    projectsGiven,
+    (v) => v.length,
+    (d) => d.year,
+  );
+  // re-calculate data
+  let newData = newRolledData.map(([year, count]) => {
+    return { ... }; // TODO
+  });
+  // re-calculate slice generator, arc data, arc, etc.
+  let newSliceGenerator = ...;
+  let newArcData = newSliceGenerator(...);
+  let newArcs = newArcData.map(...);
+  // TODO: clear up paths and legends
+  ...
+  // update paths and legends, refer to steps 1.4 and 2.2
+  ...
+}
+
+// Call this function on page load
+renderPieChart(projects);
+
+searchInput.addEventListener('change', (event) => {
+  let filteredProjects = setQuery(event.target.value);
+  // re-render legends and pie chart when event triggers
+  renderProjects(filteredProjects, projectsContainer, 'h2');
+  renderPieChart(filteredProjects);
+});

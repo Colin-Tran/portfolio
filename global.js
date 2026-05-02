7a7555949db97dd8db377699eded471b9c226138
Colin-Tran <colintran04@gmail.com>
2026-05-01 19:13:09 -0700

    commented unnecessary code out and added year to projects page

--- global.js
+++ global.js
@@ -135,9 +135,11 @@ export function renderProjects(projects, containerElement, headingLevel = "h2") 
     article.innerHTML = `
       <${headingLevel}>${title}</${headingLevel}>
       <img src="${image}" alt="${title}">
-      <p>${description}</p>
+        <div>
+          <p>${description}</p>
+          <p class="project-year">${project.year}</p>
+        </div>
     `;
-
     containerElement.appendChild(article);
   }
 }

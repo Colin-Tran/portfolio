e1b83497067f1272cbc8f32e048c9753fe181f33
Colin-Tran <colintran04@gmail.com>
2026-05-08 17:05:57 -0700

    added meta files

---
+++ meta/main.js
@@ -0,0 +1,44 @@
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
+
+async function loadData() {
+  const data = await d3.csv('loc.csv');
+  console.log(data);
+  return data;
+}
+
+function processCommits(data) {
+  return d3
+    .groups(data, (d) => d.commit)
+    .map(([commit, lines]) => {
+      let first = lines[0];
+
+      let { author, date, time, timezone } = first;
+      let datetime = new Date(first.datetime);
+
+      let ret = {
+        id: commit,
+        url: 'https://github.com/Colin-Tran/portfolio/commit/' + commit,
+        author,
+        date,
+        time,
+        timezone,
+        datetime,
+        hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
+        totalLines: lines.length,
+      };
+
+      Object.defineProperty(ret, 'lines', {
+        value: lines,
+        configurable: true,
+        writable: true,
+        enumerable: false,
+      });
+
+      return ret;
+    });
+}
+
+let data = await loadData();
+let commits = processCommits(data);
+
+console.log(commits);
